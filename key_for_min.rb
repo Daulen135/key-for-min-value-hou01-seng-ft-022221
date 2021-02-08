426513d45be7d6f70a9e396d7100bc95ed2dbe45
Daulen Zhangabylov <daulenz@gmail.com>
2021-02-08 17:16:52 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,25 +3,15 @@
 
 def key_for_min_value(name_hash)
 
-
-
+min_price=1000
+min_key=nil
 name_hash.each do|key,price|
 
 if price<min_price
-  price=min_price
-  value
+  min_price=price
+  min_key=key
 end
 end
+min_key
 end
 
-def key_for_min_value(hash)
-  lowest_key = nil
-  lowest_value = Float::INFINITY
-  hash.each do |k, v|
-    if v < lowest_value
-      lowest_value = v
-      lowest_key = k
-    end
-  end
-  lowest_key
-end
