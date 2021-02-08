add5032d2dc1d6d4279b3e3073b1d4a0bd23f320
Daulen Zhangabylov <daulenz@gmail.com>
2021-02-08 17:11:13 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,11 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+name_hash.each do|value,price|
+min_price=1000
+if price<min_price
+  price=min_price
+  value
+end
+end
 end
